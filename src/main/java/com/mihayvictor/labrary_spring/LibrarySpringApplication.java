f7576661fbc99c99664af8c6490a3967c256a46d
mihayvictor <m.victorcezario@gmail.com>
2025-10-06 21:19:07 -0300

    Correção do nome do projeto

--- src/main/java/com/mihayvictor/labrary_spring/LibrarySpringApplication.java
+++ src/main/java/com/mihayvictor/labrary_spring/LibrarySpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LabrarySpringApplication {
+public class LibrarySpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LabrarySpringApplication.class, args);
+		SpringApplication.run(LibrarySpringApplication.class, args);
 	}
 
 }
